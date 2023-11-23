17441f0b2f640e376c913a6a0a0294bb38c5dec5
duckien2012 <hoanganhtra181@gmail.com>
2023-11-23 16:13:33 +0700

    upadate

--- src/models/users.js
+++ src/models/users.js
@@ -1,17 +1,29 @@
 
 import pool from '../database.js'
 
+export async function getUsername(username) {
+    const { rows } = await pool.query('SELECT * FROM useraccount WHERE username = $1', [username]);
+    console.log(rows)
+    if(rows[0] == null)
+        return null;
+    return rows[0];
+}
+// export async function getUsername(username) {
+//     const { rows } = await pool.query('SELECT * FROM useraccount WHERE username = $1', [username]);
+//     if(rows[0] == null)
+//         return null;
+//     return rows[0];
+// }
+
 export const insertUserAccount = async (username, email, password) => {
-    const client = await pool.connect(); // connect to the pool
     try {
         // Insert data into the useraccount table
         const text = 'INSERT INTO useraccount(username, email, pass) VALUES($1, $2, $3)';
         const values = [username, email, password];
-        await client.query(text, values);
+        await pool.query(text, values);
         console.log('Data inserted successfully');
     } catch (error) {
         console.error('Error executing query', error);
-    } finally {
-        client.release(); // release the client back to the pool
-    }
+        res.status(500).send("Error creating user");
+    } 
 }
