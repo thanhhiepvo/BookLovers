4de4189e8a04cacdd70224589de92cdb4f0ee312
duckien2012 <hoanganhtra181@gmail.com>
2023-11-29 17:04:17 +0700

    login

--- src/routers/web.js
+++ src/routers/web.js
@@ -1,28 +1,16 @@
 
-// khai báo router
-
-/* The code `import express from 'express' // es module` is importing the Express framework as a
-module. This allows you to use the functionalities provided by Express in your code. The `express`
-variable is assigned the value of the imported Express module, which can then be used to create an
-instance of the Express application and define routes. */
 import express from 'express' // es module
-// .. is the parent directory of the current directory
-import { getHomePage, postCreateUser} from '../controllers/homeController.js';
-/* `const router = express.Router()` is creating a new router object using the Express framework. This
-router object can be used to define routes for specific URL paths and HTTP methods. It allows you to
-modularize your routes and separate them into different files or modules. */
+import { getHomePage, loginUser, postCreateUser} from '../controllers/homeController.js';
 const router = express.Router()
 
 //router.Method('/routers', handler function)
 router.get('/', getHomePage) ;
 
-/* The code `router.get('/login', (req, res) => {
-    res.render('login.ejs')
-})` is defining a route for the GET HTTP method with the URL path '/login'. */
 router.get('/login', (req, res) => {
-    res.render('login.ejs')
+    res.render('login.ejs', { message: req.flash('msg') })
 })
-  
+router.post('/login-user', loginUser);
+
 router.get('/forgotPass', (req, res) => {
     res.render('forgotPass.ejs')
 })
@@ -42,12 +30,11 @@ router.get('/myBook', (req, res) => {
 router.get('/signUp', (req, res) => {
     res.render('signUp.ejs')
 })
-
 router.post('/create-user', postCreateUser);
   
 router.get('/homepage', (req, res) => {
       //res.send('Check out our homepage') // send text
-      res.render('home.ejs') // res.render() to render a template file : tạo view dynamic 
+      res.render('home.ejs'); // res.render() to render a template file : tạo view dynamic 
 })
 
 router.get('/wallet', (req, res) => {
