43ac654546e1b42f614fbef28daa2674a7b6551c
Hoang Xuan Khoi <khoihoangxuan1508@gmail.com>
2023-12-15 19:04:53 +0700

    update book page

--- src/routers/web.js
+++ src/routers/web.js
@@ -13,7 +13,7 @@ const router = express.Router()
 
 router.get('/login', (req, res) => {
     res.render('login.ejs', { message: req.flash('msg') })
-    console.log(">>> req.session.username = ", req.session.username);
+    // console.log(">>> req.session.username = ", req.session.username);
 })
 router.post('/login-user', authenController.loginUser);
 
@@ -43,14 +43,14 @@ router.get('/about', async (req, res) => {
     }
 })
 
-router.get('/book', async (req, res) => {
-    if (req.session.username) {
-        const user = await authenController.getProfileUser(req, res);
-        res.render('book', { user: user });
-    } else {
-        res.redirect('/login');
-    }
-})
+// router.get('/book', async (req, res) => {
+//     if (req.session.username) {
+//         const user = await authenController.getProfileUser(req, res);
+//         res.render('book', { user: user });
+//     } else {
+//         res.redirect('/login');
+//     }
+// })
 
 // router.get('/editProfile', (req, res) => {
 //     res.render('editProfile.ejs')
@@ -94,7 +94,7 @@ router.get('/myBook', async (req, res) => {
     } else {
         res.redirect('/login');
     }
-    console.log(">>> req.session.username = ", req.session.username);
+    // console.log(">>> req.session.username = ", req.session.username);
 })
 
 router.get('/selling', async (req, res) => {
@@ -112,7 +112,7 @@ router.get('/selling', async (req, res) => {
     } else {
         res.redirect('/login');
     }
-    console.log(">>> req.session.username = ", req.session.username);
+    // console.log(">>> req.session.username = ", req.session.username);
 })
 router.get('/upload', async (req, res) => {
     if (req.session.username) {
@@ -141,7 +141,27 @@ router.get('/editProfile', async (req, res) => {
     }
 })
 router.post('/edit-profile', profileController.updateInfo)
-router.get('/book', profileController.getInfo);
+// router.get('/book', profileController.getInfo);
+
+router.get('/book', async (req, res) => {
+    if (req.session.username) {
+        try {
+            const book = await bookController.getInfoOneBook(req, res);
+            const user = await authenController.getProfileUser(req, res);
+            //books = book;
+            res.render('book', {
+                user: user,
+                books: book
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    } 
+    else {
+        res.redirect('/login');
+    }
+    // console.log(">>> req.session.username = ", req.session.username);
+});
 
 router.get('/logout', authenController.logout);
 
