83ea2dd2c96cab8a391ea542582a1fa788e2fbc2
tdphong1293 <ducphongtang1293@gmail.com>
2024-01-07 14:38:39 +0700

    xoa file

--- src/controllers/sellingController.js
+++
@@ -1,8 +0,0 @@
-const sellingController = {};
-
-sellingController.upLoadBook = async (req, res) => {
-    let { nameBook, author, price, description, publishedYear, uploadImage, uploadPDF } = req.body;
-    username = req.session.username;
-}
-
-export default sellingController;
